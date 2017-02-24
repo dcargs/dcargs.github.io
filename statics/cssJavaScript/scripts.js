55179bf32a4c0b5c9e2c9e1ff46b5a4cbddf07b9
Devin Cargill <devincargill@Devins-MacBook-Pro.local>
2017-02-23 22:52:19 -0600

    cleaning up whitespace

--- statics/cssJavaScript/scripts.js
+++ statics/cssJavaScript/scripts.js
@@ -4,11 +4,9 @@ function updateClock ( )
   	var currentHours = currentTime.getHours ( );
   	var currentMinutes = currentTime.getMinutes ( );
   	var currentSeconds = currentTime.getSeconds ( );
-
   	// Pad the minutes and seconds with leading zeros, if required
   	currentMinutes = ( currentMinutes < 10 ? "0" : "" ) + currentMinutes;
   	currentSeconds = ( currentSeconds < 10 ? "0" : "" ) + currentSeconds;
-
   	// Choose either "AM" or "PM" as appropriate
   	var timeOfDay = ( currentHours < 12 ) ? "AM" : "PM";
     if(timeOfDay == "AM"){
@@ -16,20 +14,13 @@ function updateClock ( )
     } else if(timeOfDay == "PM"){
         $("#greeting").html("Good evening, Devin");
       }
-
   	// Convert the hours component to 12-hour format if needed
   	currentHours = ( currentHours > 12 ) ? currentHours - 12 : currentHours;
-
   	// Convert an hours component of "0" to "12"
   	currentHours = ( currentHours == 0 ) ? 12 : currentHours;
-
   	// Compose the string for display
   	var currentTimeString = currentHours + ":" + currentMinutes + ":" + currentSeconds + " " + timeOfDay;
-
-
    	$("#clock").html(currentTimeString);
-
-
  }
 
 $(document).ready(function()
