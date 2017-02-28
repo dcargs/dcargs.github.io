0a36e6084d64c974379549452e84bae59bc94d05
Devin Cargill <devincargill@Devins-MacBook-Pro.local>
2017-02-28 10:49:28 -0600

    Added completed columns

--- statics/cssJavaScript/scripts.js
+++ statics/cssJavaScript/scripts.js
@@ -12,7 +12,7 @@ function updateClock ( )
     if(timeOfDay == "AM"){
       $("#greeting").html("Good morning, Devin: ");
     } else if(timeOfDay == "PM"){
-        $("#greeting").html("Good evening, Devin ");
+        $("#greeting").html("Good evening, Devin: ");
       }
   	// Convert the hours component to 12-hour format if needed
   	currentHours = ( currentHours > 12 ) ? currentHours - 12 : currentHours;
@@ -20,10 +20,77 @@ function updateClock ( )
   	currentHours = ( currentHours == 0 ) ? 12 : currentHours;
   	// Compose the string for display
   	var currentTimeString = currentHours + ":" + currentMinutes + ":" + currentSeconds + " " + timeOfDay;
-   	$("#clock").html("- " + currentTimeString);
+   	$("#clock").html(currentTimeString);
  }
 
 $(document).ready(function()
 {
    setInterval('updateClock()', 1000);
+   document.getElementById("copyButtonHome").addEventListener("click", function() {
+       copyToClipboard(document.getElementById("copyTargetHome"));
+   });
+   document.getElementById("copyButtonCS2830").addEventListener("click", function() {
+       copyToClipboard(document.getElementById("copyTargetCS2830"));
+   });
+   document.getElementById("copyButtonCS3380").addEventListener("click", function() {
+       copyToClipboard(document.getElementById("copyTargetCS3380"));
+   });
+   document.getElementById("copyButtonTCI").addEventListener("click", function() {
+       copyToClipboard(document.getElementById("copyTargetTCI"));
+   });
+   document.getElementById("copyButtonHenryWebsite").addEventListener("click", function() {
+       copyToClipboard(document.getElementById("copyTargetHenryWebsite"));
+   });
 });
+
+
+
+function copyToClipboard(elem) {
+	  // create hidden text element, if it doesn't already exist
+    var targetId = "_hiddenCopyText_";
+    var isInput = elem.tagName === "INPUT" || elem.tagName === "TEXTAREA";
+    var origSelectionStart, origSelectionEnd;
+    if (isInput) {
+        // can just use the original source element for the selection and copy
+        target = elem;
+        origSelectionStart = elem.selectionStart;
+        origSelectionEnd = elem.selectionEnd;
+    } else {
+        // must use a temporary form element for the selection and copy
+        target = document.getElementById(targetId);
+        if (!target) {
+            var target = document.createElement("textarea");
+            target.style.position = "absolute";
+            target.style.left = "-9999px";
+            target.style.top = "0";
+            target.id = targetId;
+            document.body.appendChild(target);
+        }
+        target.textContent = elem.textContent;
+    }
+    // select the content
+    var currentFocus = document.activeElement;
+    target.focus();
+    target.setSelectionRange(0, target.value.length);
+
+    // copy the selection
+    var succeed;
+    try {
+    	  succeed = document.execCommand("copy");
+    } catch(e) {
+        succeed = false;
+    }
+    // restore original focus
+    if (currentFocus && typeof currentFocus.focus === "function") {
+        currentFocus.focus();
+    }
+
+    if (isInput) {
+        // restore prior selection
+        elem.setSelectionRange(origSelectionStart, origSelectionEnd);
+    } else {
+        // clear temporary content
+        target.textContent = "";
+    }
+    return succeed;
+}
